1dfef8a1985795d4fde1d47b867da72dd5b6587f
Felipe <felipe@pop-os.localdomain>
2022-01-28 11:49:56 -0300

    deploy up

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.Promise = global.Promise;
 
-const MONGODB_URL = process.env.MONGO_URL;
+const MONGODB_URL =
+  'mongodb+srv://' +
+  process.env.DB_USER +
+  ':' +
+  process.env.DB_PASSOWRD +
+  '@cluster0.qenjw.mongodb.net/' +
+  process.env.DB_DATA_BASE +
+  '?retryWrites=true&w=majority';
 
 mongoose
   .connect(MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log('Connection succesfull'))
+  .then(db => console.log('Connection succesfull' + db.connect()))
   .catch(err => console.log(err));
