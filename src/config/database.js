988c97d211d7b1d9b84afc19a6bbf071e44230f9
Felipe <felipe@pop-os.localdomain>
2022-01-27 21:06:47 -0300

    heroku deploy

--- src/config/database.js
+++ src/config/database.js
@@ -4,13 +4,12 @@ mongoose.Promise = global.Promise;
 
 require('dotenv').config();
 
-const MONGO_URL = process.env.MONGO_URL;
+const MONGODB_URL = process.env.MONGO_URL;
 
 mongoose
-  .connect(MONGO_URL, {
+  .connect(MONGODB_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
   })
   .then(() => console.log('Connection succesfull'))
   .catch(err => console.log(err));
